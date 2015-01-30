4c95288ef332c6ebcd3e218a10232674102b3e42
purepennons <y80811@gmail.com>
2015-01-30 15:29:20 +0800

    write create docker image function

--- dockerLargeMulti.sh
+++ dockerLargeMulti.sh
@@ -1,31 +1,67 @@
 #!/bin/bash
-#counter=2
-#sudo ls -l
-#containerID=$(sudo docker ps -a -q)
-#counter=$(($counter+1))
 
 read times
 
-for i in $(seq 1 $times)
-do
-  if [ "$i" -eq 1 ]; then
-    sudo docker run ubuntu:14.04 mkdir test$i
-    sudo docker commit -m="test$i" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/test$i:$i
+limit=3
 
-  else
-    prev=$(($i-1))
+dockerBatchCreate() {
+  baseImage=$1
+  from=$2
+  num=$3
 
-    sudo docker run chiahao/test$prev:$prev mkdir test$i
-    #imageId=$($($(sudo docker images -q):0:11))
+  end=$(($from+$num))
 
-    if [ "$i" -eq "$times" ]; then
-      sudo docker commit -m="$times" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/final$times:final
-    else
+  for i in $(seq $from $end)
+  do
+    if [ "$i" -eq "$from" ]; then
+      sudo docker run $baseImage mkdir test$i
       sudo docker commit -m="test$i" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/test$i:$i
+
+    else
+      prev=$(($i-1))
+
+      sudo docker run chiahao/test$prev:$prev mkdir test$i
+
+      if [ "$i" -eq "$end" ]; then
+        sudo docker commit -m="$end" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/final$end:final
+      else
+        sudo docker commit -m="test$i" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/test$i:$i
+      fi
+
     fi
 
-  fi
-done
+  done
+
+  imageId="$(sudo docker images -q)"
+  echo $imageId
+  local arrayId=($imageId)
+  echo "${arrayId[0]}"
+
+}
+
+#imageId=$(dockerBatchCreate "ubuntu:14.04" 1 times)
+
+if [ "$times" -lt "$(($limit+1))" ]; then
+  imageId=$(dockerBatchCreate "ubuntu:14.04" 1 times)
+
+else
+
+  #for i in $(seq 1 $times)
+  for ((i=1; i<="$times"; i=i+$limit)); do
+
+    firstImageId=""
+
+    if [ "$i" -eq "1" ]; then
+
+      firstImageId=$(dockerBatchCreate "ubuntu:14.04" 1 $limit)
+
+    else
+      echo "else"
+    fi
+
+  done
+
+fi
 
 
 exit 0

f8c73abda7158d8f465cd7ce902bacc1e0ddc648
purepennons <y80811@gmail.com>
2015-01-31 16:05:30 +0800

    let program support more than 127 layers

--- dockerLargeMulti.sh
+++ dockerLargeMulti.sh
@@ -5,22 +5,33 @@ read times
 limit=3
 
 dockerBatchCreate() {
-  baseImage=$1
-  from=$2
-  num=$3
+  local baseImage=$1
+  local from=$2
+  local end=$3
 
-  end=$(($from+$num))
+  # local end=$(($from+$num-1))
+
+  # local end=$(($num-$from+1))
 
   for i in $(seq $from $end)
   do
     if [ "$i" -eq "$from" ]; then
       sudo docker run $baseImage mkdir test$i
+
+      wait
+
       sudo docker commit -m="test$i" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/test$i:$i
+
+      wait
 
     else
       prev=$(($i-1))
 
       sudo docker run chiahao/test$prev:$prev mkdir test$i
+
+      # sleep 1s
+
+      wait
 
       if [ "$i" -eq "$end" ]; then
         sudo docker commit -m="$end" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/final$end:final
@@ -28,35 +39,57 @@ dockerBatchCreate() {
         sudo docker commit -m="test$i" -a="Chiahao Lin" $(sudo docker ps -l -q) chiahao/test$i:$i
       fi
 
+      wait
+
     fi
 
   done
 
-  imageId="$(sudo docker images -q)"
-  echo $imageId
-  local arrayId=($imageId)
-  echo "${arrayId[0]}"
+  # local imageIds="$(sudo docker images -q)"
+  # local imageId="$(($imageIds | sed -n 1p))"
+  # echo -e "$imageId\r"
+
+  # local arrayId=($imageId)
+  # echo "${arrayId[0]}"
 
 }
 
-#imageId=$(dockerBatchCreate "ubuntu:14.04" 1 times)
-
 if [ "$times" -lt "$(($limit+1))" ]; then
-  imageId=$(dockerBatchCreate "ubuntu:14.04" 1 times)
+  dockerBatchCreate "ubuntu:14.04" "1" "$times"
+
 
 else
 
-  #for i in $(seq 1 $times)
-  for ((i=1; i<="$times"; i=i+$limit)); do
+  for ((k=1; k<="$times"; k=k+$limit)); do
 
-    firstImageId=""
 
-    if [ "$i" -eq "1" ]; then
+    if [ "$k" -eq "1" ]; then
 
-      firstImageId=$(dockerBatchCreate "ubuntu:14.04" 1 $limit)
+      dockerBatchCreate "ubuntu:14.04" "1" "$limit"
+
+      wait
 
     else
-      echo "else"
+
+      parEnd=""
+
+      if [ "$(($k+$limit))" -le "$times" ]; then
+        parEnd=$(($k+$limit))
+      else
+        parEnd=$times
+      fi
+
+
+      prevImageId="$(sudo docker images -q | sed -n 1p)"
+
+      wait
+
+      echo "k: $k end: $parEnd id: $prevImageId"
+
+      dockerBatchCreate "$prevImageId" "$k" "$parEnd"
+
+      wait
+
     fi
 
   done
